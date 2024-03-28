1bf7c1a69b739668b2449d0c04810fb25fb3733e
Sandeep Nair <nair.sandeep@microsoft.com>
2024-03-28 16:22:57 -0700

    Initial Commit

---
+++ metricsVisualizer/Services/CDBMetricService.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using static Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary;
+
+namespace CDBInstantMetricsVisualizer.Data
+{
+    public class CDBMetricService
+    {
+        public Dictionary<string, List<double>> MetricsList;
+        public DateTime StartDatetime;
+        CosmosDBService cdb;
+
+
+        public void Init(CosmosDBService cdb)
+        {
+            ResetData();
+            this.cdb = cdb;
+        }
+
+        public void ResetData()
+        {
+            MetricsList = new Dictionary<string, List<double>>();
+
+            if (cdb != null)
+                cdb.DashboardMetrics.Clear();
+
+            StartDatetime = DateTime.Now.AddMinutes(-2);
+        }
+
+        public async Task UpdateMetricsAsync(MetricType mtype, List<string> xAxislabels)
+        {
+            try
+            {
+                //making sure there is 2 minutes data
+                DateTime endDt = DateTime.Now;
+                if (endDt.Subtract(StartDatetime).TotalSeconds > 120)
+                    StartDatetime = endDt.AddSeconds(-120);
+
+                int counter = 0;
+                while (StartDatetime.AddSeconds(counter) <= endDt)
+                {
+                    string timeLookupKey;
+
+                    //look if this metric exists for given time.
+                    timeLookupKey = StartDatetime.AddSeconds(counter).ToString("yyyy-MM-dd HH:mm:ss");
+                    xAxislabels.Add(StartDatetime.AddSeconds(counter).ToString("HH:mm:ss"));
+
+                    List<string> metricKeys = new List<string>();
+                    CDBMetricService.MetricAggregationType aggType = CDBMetricService.MetricAggregationType.Count;
+                    switch (mtype)
+                    {
+                        case MetricType.RU:
+                            aggType = CDBMetricService.MetricAggregationType.Sum;
+                            metricKeys = cdb.UniqueFunctionNames;
+                            break;
+                        case MetricType.Latency:
+                            aggType = CDBMetricService.MetricAggregationType.Average;
+                            metricKeys = cdb.UniqueFunctionNames;
+                            break;
+                        case MetricType.StatusCode:
+                            aggType = CDBMetricService.MetricAggregationType.Count;
+                            metricKeys = cdb.UniqueStatusCode;
+                            break;
+                    }
+
+                    foreach (var metrickey in metricKeys)
+                    {
+                        //checking if metric name is new entry
+                        if (!MetricsList.ContainsKey(metrickey))
+                        {
+                            MetricsList.Add(metrickey, new List<double>());
+
+                            //add leading 0 to make sure  chart data is consistent
+                            int secondsPassed = (int)endDt.Subtract(StartDatetime).TotalSeconds;
+                            List<double> blankList = new List<double>(new double[secondsPassed]);
+                            MetricsList[metrickey] = blankList;
+                        }
+
+
+                        double calculatedValue = 0;//;
+                        if (cdb.DashboardMetrics.ContainsKey(timeLookupKey)) //if exists in metrics
+                        {
+                            var metrics = cdb.DashboardMetrics[timeLookupKey];
+                            calculatedValue = CalcLineMetricValue(metrics, metrickey, aggType);
+                        }
+                        else
+                        {
+                            calculatedValue = 0;
+                        }
+                        //set value for  the given second
+                        MetricsList[metrickey].Add(calculatedValue);
+
+
+                        //ensure only 120 items are stored.
+                        int listCount = MetricsList[metrickey].Count;
+                        if (listCount > 120)
+                        {
+                            MetricsList[metrickey].RemoveRange(0, listCount - 120);
+                        }
+                    }
+                    counter++;
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private double CalcLineMetricValue(List<CDBMetric> metrics, string metrickey, MetricAggregationType aggType )
+        {
+            double calcValue = 0;
+            List<double> latency_values = new List<double>();//;
+            switch (aggType)
+            {
+                case MetricAggregationType.Sum:
+                     foreach (CDBMetric m in metrics)
+                    {
+                        if (m.metricName == metrickey) //only adding for current metricname
+                        {
+                            calcValue = calcValue + m.RU;
+                        }
+                    }
+                    return calcValue; 
+                case MetricAggregationType.Average:
+                    foreach (CDBMetric m in metrics)
+                    {
+                        if (m.metricName == metrickey) //only adding for current metricname
+                        {
+                            latency_values.Add(m.Latency);
+                        }
+                        if(latency_values.Count > 0)
+                            calcValue = latency_values.Average();
+                        else
+                            calcValue = 0;
+
+                    }
+                    return calcValue;
+                case MetricAggregationType.Count:
+                    foreach (CDBMetric m in metrics)
+                    {
+                        if (m.StatusCode == metrickey) //only adding for current metricname
+                        {
+                            calcValue++;
+                        }
+                    }
+                    return calcValue;
+            }
+
+            return 0;
+        }
+
+
+        public enum MetricType {RU,Latency,StatusCode};
+        public enum MetricAggregationType { Sum, Average, Count };
+
+    }
+}
---
+++ metricsVisualizer/Services/CosmosDBService.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.NetworkInformation;
+using System.Runtime;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using System.Configuration;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Net;
+using System.ComponentModel;
+using Container = Microsoft.Azure.Cosmos.Container;
+using System.Drawing;
+
+namespace CDBInstantMetricsVisualizer.Data
+{
+    public class CosmosDBService
+    {
+        Database? database;
+        Container? container;
+        Container? leaseContainer;
+
+        string DatabaseName;
+        string ContainerName;
+        CosmosClient cosmosClient;
+
+        string pkToMonitor;
+
+        bool changeFeedInitialized = false; 
+
+        public Dictionary<string, List<CDBMetric>> DashboardMetrics=new Dictionary<string, List<CDBMetric>>();
+        public List<string> UniqueFunctionNames=new List<string>();
+        public List<string> UniqueStatusCode = new List<string>();
+
+        public CosmosDBService(string Endpoint, string Key, string Database, string Container,string preferredregions)
+        {
+            // Configure CosmosClientOptions
+            var clientOptions = new CosmosClientOptions
+            {
+                SerializerOptions = new CosmosSerializationOptions
+                {
+                    IgnoreNullValues = true,
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+                },
+                ApplicationName = "ChatFrontEnd",
+                ConnectionMode = ConnectionMode.Direct,
+                ApplicationPreferredRegions = preferredregions.Split(','),
+
+
+
+            };
+
+            // Create a CosmosClient using the endpoint and key
+            cosmosClient = new CosmosClient(Endpoint, Key, clientOptions);
+            database = cosmosClient?.GetDatabase(Database);
+            container = database?.GetContainer(Container);
+            leaseContainer = database?.GetContainer("leases");
+
+            DatabaseName = Database;
+            ContainerName= Container;
+
+        }
+
+        public void initChnageFeed(string pk)
+        {
+            if(changeFeedInitialized) return;
+
+            changeFeedInitialized = true;
+
+            pkToMonitor = pk;
+
+            ChangeFeedProcessor changeFeedProcessor = cosmosClient.GetContainer(DatabaseName, ContainerName)
+             .GetChangeFeedProcessorBuilder<CDBMetric>("MetricsDashboard", HandleChangesAsync)
+                 .WithInstanceName("MetricsDashboard")
+                 .WithLeaseContainer(leaseContainer) 
+                 .WithStartTime(DateTime.Now)
+                 .Build();
+
+            DashboardMetrics = new Dictionary<string, List<CDBMetric>>();
+            changeFeedProcessor.StartAsync();
+        }
+
+        async Task HandleChangesAsync(ChangeFeedProcessorContext context, IReadOnlyCollection<CDBMetric> changes, CancellationToken cancellationToken)
+        {
+            foreach (var change in changes)
+            {
+                if (pkToMonitor == change.pk)
+                {
+                    string  TimeInSec=change.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss");
+                    if (!DashboardMetrics.ContainsKey(TimeInSec))                   
+                    {
+                        DashboardMetrics.Add(TimeInSec, new List<CDBMetric>());
+                    }
+                    DashboardMetrics[TimeInSec].Add(change);
+
+                    if(!UniqueFunctionNames.Contains(change.metricName))
+                        UniqueFunctionNames.Add(change.metricName);
+
+                    if (!UniqueStatusCode.Contains(change.StatusCode))
+                        UniqueStatusCode.Add(change.StatusCode);
+                }
+            }
+        }
+       
+      
+
+    }
+
+
+    public record CDBMetric(
+            string id,
+            string pk,
+            string metricName,
+            double RU,
+            string StatusCode,
+            double Latency,
+            DateTime TimeStamp
+        );
+
+
+}
